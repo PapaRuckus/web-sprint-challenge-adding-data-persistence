aa3244545204b81af758ab8474de6c78da580fb6
Calvin K <calvinknaub@gmail.com>
2023-12-28 12:16:19 -0800

    finished resource model/router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,20 @@
 // build your `/api/resources` router here
+const express = require("express");
+const router = express.Router();
+const Resource = require("./model");
+
+router.get("/", (req, res, next) => {
+    Resource.getResources().then(resource => {
+      res.status(200).json(resource)
+  }).catch(next)
+});
+
+router.post("/", (req, res, next) => {
+  Resource.createResource(req.body)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
 const projectsRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
 
 
 const server = express()
 
 server.use(express.json())
 server.use('/api/projects', projectsRouter)
+server.use("/api/resources", resourceRouter);
 
 // server.get('*', (req, res, next) => {
 //     res.status(200).json('welcome to the main page')
